932a3f959524066eefa66f257c5d05058dff3523
Litonuddinnil <mdniloyhasan544@gmail.com>
2024-10-17 18:59:59 +0600

    creating and nesting component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import './App.css'
+import MyApp from './Myapp'
+ 
+
+function App() {
+
+  return (
+    <>
+      <h1>Creating and nesting component</h1>
+      <MyApp></MyApp>
+  
+    </>
+  )
+}
+
+export default App
